5c07c611a228d6eb2b111daad95456b661308320
MaxSJimenez04 <maximiliano.soto.j@gmail.com>
2026-05-31 21:30:23 -0600

    feat: Agregar vistas, hoja de estilos y fuentes de texto

---
+++ vistas/wInicio.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClienteAhorcado.vistas
+{
+    /// <summary>
+    /// Lógica de interacción para wInicio.xaml
+    /// </summary>
+    public partial class wInicio : Page
+    {
+        public wInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJugar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOpciones_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
